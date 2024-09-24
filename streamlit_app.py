0130a8837a5098ad67f059385eb799102f86bc49
HerlambangNaufal <90168283+HerlambangNaufal@users.noreply.github.com>
2024-09-25 01:10:41 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -408,9 +408,9 @@ def main():
                     X_train = vectorizer.fit_transform(X_train)
                     X_test = vectorizer.transform(X_test)
 
-                    clf_rbf = svm.SVC(kernel='rbf')
-                    clf_rbf.fit(X_train, Y_train)
-                    predict = clf_rbf.predict(X_test)
+                    clf_poly = svm.SVC(kernel='poly')
+                    clf_poly.fit(X_train, Y_train)
+                    predict = clf_poly.predict(X_test)
 
                     st.write("SVM Accuracy score  -> ", accuracy_score(predict, Y_test)*100)
                     st.write("SVM Recall Score    -> ", recall_score(predict, Y_test, average='macro')*100)
